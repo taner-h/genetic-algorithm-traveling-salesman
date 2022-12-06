c44ac60878f0f27af68837c18100c288ff1662d0
Taner Hacıoğlu <tanerfb1999@gmail.com>
2022-12-07 00:41:54 +0300

    Individual class added

--- genetic.py
+++ genetic.py
@@ -1,75 +1,107 @@
 import random
 import numpy as np
 
-def generateSoldItemCount(count):
-    while True:
-        items = [random.random() for _ in range(5)]
-        total = sum(items)
-         # round() method ensures that elements are natural number (Hard Constraint #1)
-        items = [round(count * item / total) for item in items]
-        # Make sure sold item amounts is equal to the stock (Hard Constraint #3)
-        if sum(items) == count:
-            return items
+class Individual:
+    def __init__(self):
+        self.state = self.generateRandomIndividual()
+        self.transposed = np.transpose(self.state)
+        self.revenueMatrix = self.calculateRevenueMatrix()
+        self.revenueMatrixTransposed = np.transpose(self.revenueMatrix)
+        self.fbase = self.calculateTotalRevenue()
+        self.f1 = self.calculateF1Bonus()
+        self.f2 = self.calculateF2Bonus()
+        self.f3 = self.calculateF3Bonus()
+        self.f = self.fbase + self.f1 + self.f2 + self.f3
 
-def generateRandomIndividual():
-    while True:
-        x1 = generateSoldItemCount(30)
-        x2 = generateSoldItemCount(40)
-        x3 = generateSoldItemCount(20)
-        x4 = generateSoldItemCount(40)
-        x5 = generateSoldItemCount(20)
+    def calculateRevenueMatrix(self):
+        return self.state * priceMatrix
 
-        state = np.array([x1, x2, x3, x4, x5])
-        # Make sure that exactly 150 items is sold (Hard Constraint #2)
-        if is150ItemsSold(state):  
-            return state
+    def calculateTotalRevenue(self):
+        return sum(self.revenueMatrix.flatten())
 
-def printIndividualState(state):
-    print('\tx1\tx2\tx3\tx4\tx5')
-    print('A\t'+'\t'.join(str(num) for num in state[0]))
-    print('B\t'+'\t'.join(str(num) for num in state[1]))
-    print('C\t'+'\t'.join(str(num) for num in state[2]))
-    print('D\t'+'\t'.join(str(num) for num in state[3]))
-    print('E\t'+'\t'.join(str(num) for num in state[4]))
+    def generateSoldItemCount(self, count):
+        while True:
+            items = [random.random() for _ in range(5)]
+            total = sum(items)
+            # round() method ensures that elements are natural number (Hard Constraint #1)
+            items = [round(count * item / total) for item in items]
+            # Make sure sold item amounts is equal to the stock (Hard Constraint #3)
+            if sum(items) == count:
+                return items
+
+    def generateRandomIndividual(self):
+        while True:
+            x1 = self.generateSoldItemCount(30)
+            x2 = self.generateSoldItemCount(40)
+            x3 = self.generateSoldItemCount(20)
+            x4 = self.generateSoldItemCount(40)
+            x5 = self.generateSoldItemCount(20)
+
+            state = np.array([x1, x2, x3, x4, x5])
+            # Make sure that exactly 150 items is sold (Hard Constraint #2)
+            if self.is150ItemsSold(state):  
+                return state
+
+    def printIndividualState(self):
+        print('\tx1\tx2\tx3\tx4\tx5')
+        print('A\t'+'\t'.join(str(num) for num in self.state[0]))
+        print('B\t'+'\t'.join(str(num) for num in self.state[1]))
+        print('C\t'+'\t'.join(str(num) for num in self.state[2]))
+        print('D\t'+'\t'.join(str(num) for num in self.state[3]))
+        print('E\t'+'\t'.join(str(num) for num in self.state[4]))
+
+    def is150ItemsSold(self,state):
+        return sum(state.flatten()) == 150 
+
+    def calculateF1Bonus(self):
+        if self.isAllCitiesVisited(): return 100
+        return 0
+
+    def calculateF2Bonus(self):
+        # transposed = np.transpose(state)
+        # revenue = np.transpose(calculateRevenueMatrix(state))
+        bonuses = []
+        for index, city in enumerate(self.transposed):
+            cityRevenue = sum(self.revenueMatrixTransposed[index])
+            difference = (max(city) - min(city))
+            bonusPercentage = max(20 - difference, 0)
+            bonus = bonusPercentage * cityRevenue / 100
+            bonuses.append(bonus)
+        return sum(bonuses)
+
+    def calculateF3Bonus(self):
+        # transposed = np.transpose(state)
+        # fbase = calculateTotalRevenue(state)
+        itemsSoldPerCity = [sum(city) for city in self.transposed]
+        difference = max(itemsSoldPerCity) - min(itemsSoldPerCity)
+        bonusPercentage = max(20 - difference, 0)
+        bonus = bonusPercentage * self.fbase / 100 
+        return bonus
+
+    def isAllCitiesVisited(self):
+        # transposed = np.transpose(state)
+        for city in self.transposed:
+            if sum(city) == 0: return False 
+        return True 
+
+    def printIndividualProperties(self):
+        print('Individual State:')
+        self.printIndividualState()
+        print('Matrix:\n', self.state)
+        print('Transposed:\n', self.transposed)
+        print('Revenue Matrix:\n', self.revenueMatrix)
+        print('fbase:', self.fbase)
+        print('f1:', self.f1)
+        print('f2:', self.f2)
+        print('f3:', self.f3)
+        print('f:', self.f)
+
+
 
 def printPopulation(population):
     for individual in population:
         printIndividualState(individual)
         print()
-
-def is150ItemsSold(state):
-    return sum(state.flatten()) == 150 
-
-
-def isAllCitiesVisited(state):
-    transposed = np.transpose(state)
-    for city in transposed:
-        if sum(city) == 0: return False 
-    return True 
-
-def calculateF1Bonus(state):
-    if isAllCitiesVisited(state): return 100
-    return 0
-
-def calculateF2Bonus(state):
-    transposed = np.transpose(state)
-    revenue = np.transpose(calculateRevenueMatrix(state))
-    bonuses = []
-    for index, city in enumerate(transposed):
-        cityRevenue = sum(revenue[index])
-        difference = (max(city) - min(city))
-        bonusPercentage = max(20 - difference, 0)
-        bonus = bonusPercentage * cityRevenue / 100
-        bonuses.append(bonus)
-    return bonuses, sum(bonuses)
-
-def calculateF3Bonus(state):
-    transposed = np.transpose(state)
-    fbase = calculateTotalRevenue(state)
-    itemsSoldPerCity = [sum(city) for city in transposed]
-    difference = max(itemsSoldPerCity) - min(itemsSoldPerCity)
-    bonusPercentage = max(20 - difference, 0)
-    bonus = bonusPercentage * fbase / 100 
 
 def generatePriceMatrix():
     return np.array([
@@ -80,22 +112,21 @@ def generatePriceMatrix():
         [10,5,12,6,3],
     ])
 
-def calculateRevenueMatrix(state):
-    return state * priceMatrix
-
-def calculateTotalRevenue(state):
-    return sum(calculateRevenueMatrix(state).flatten())
 
 N = 8 # population size
-population = [generateRandomIndividual() for _ in range(N)]
 priceMatrix = generatePriceMatrix()
+
+# population = [Individual() for _ in range(N)]
+node = Individual()
+node.printIndividualProperties()
+
 # printPopulation(population)
-print(population[0])
+
 # print(calculateRevenueMatrix(population[0]))
 # print(calculateF1Bonus(population[0]))
 # print(calculateF2Bonus(population[0]))
 
-calculateF3Bonus(population[0])
+
 
 # x = generateRandomIndividual()
 # print(x)
