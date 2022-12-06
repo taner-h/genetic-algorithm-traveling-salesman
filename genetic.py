c24954822e7cb230f547c1f9b780605c27bed2ac
Taner Hacıoğlu <tanerfb1999@gmail.com>
2022-12-06 21:55:34 +0300

    changed state to numpy matrix

--- genetic.py
+++ genetic.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 def generateSoldItemCount(count):
     while True:
@@ -18,45 +19,36 @@ def generateRandomIndividual():
         x4 = generateSoldItemCount(40)
         x5 = generateSoldItemCount(20)
 
-        state = x1 + x2 + x3 + x4 + x5
-
+        state = np.array([x1, x2, x3, x4, x5])
         # Make sure that exactly 150 items is sold (Hard Constraint #2)
         if is150ItemsSold(state):  
             return state
 
 def printIndividualState(state):
     print('\tx1\tx2\tx3\tx4\tx5')
-    print('A\t', end='')
-    print('\t'.join(str(num) for num in state[:5]))
-    print('B\t', end='')
-    print('\t'.join(str(num) for num in state[5:10]))
-    print('C\t', end='')
-    print('\t'.join(str(num) for num in state[10:15]))
-    print('D\t', end='')
-    print('\t'.join(str(num) for num in state[15:20]))
-    print('E\t', end='')
-    print('\t'.join(str(num) for num in state[20:]))
-
+    print('A\t'+'\t'.join(str(num) for num in state[0]))
+    print('B\t'+'\t'.join(str(num) for num in state[1]))
+    print('C\t'+'\t'.join(str(num) for num in state[2]))
+    print('D\t'+'\t'.join(str(num) for num in state[3]))
+    print('E\t'+'\t'.join(str(num) for num in state[4]))
 
 def printPopulation(population):
     for individual in population:
-        print(individual)
         printIndividualState(individual)
         print()
 
 def is150ItemsSold(state):
-    return sum(state) == 150 
+    return sum(state.flatten()) == 150 
 
-def getX1(state):
-    return [state[0], state[5], state[10], state[15], state[20]] 
 
-# def isAllCitiesVisited(state):
 
 
 N = 8 # population size
 population = [generateRandomIndividual() for _ in range(N)]
 printPopulation(population)
 
-print(getX1(population[7]))
 
-
+# x = generateRandomIndividual()
+# print(x)
+# print(np.transpose(x))
+# printIndividualState(x)
