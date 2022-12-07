2c060f9aa43695d50ebf9cb7cc785bfd8ea1ba86
Taner Hacıoğlu <tanerfb1999@gmail.com>
2022-12-07 21:41:41 +0300

    changes

--- genetic.py
+++ genetic.py
@@ -20,18 +20,28 @@ class Individual:
         return sum(self.revenueMatrix.flatten())
 
     def printState(self):
-        print('\tx1\tx2\tx3\tx4\tx5')
-        print('A\t'+'\t'.join(str(num) for num in self.state[0]))
-        print('B\t'+'\t'.join(str(num) for num in self.state[1]))
-        print('C\t'+'\t'.join(str(num) for num in self.state[2]))
-        print('D\t'+'\t'.join(str(num) for num in self.state[3]))
-        print('E\t'+'\t'.join(str(num) for num in self.state[4]))
+        print('\tx1\tx2\tx3\tx4\tx5\tTotal')
+        print('A\t'+'\t'.join(str(num) for num in self.state[0])+'\t'+str(sum(self.state[0])))
+        print('B\t'+'\t'.join(str(num) for num in self.state[1])+'\t'+str(sum(self.state[1])))
+        print('C\t'+'\t'.join(str(num) for num in self.state[2])+'\t'+str(sum(self.state[2])))
+        print('D\t'+'\t'.join(str(num) for num in self.state[3])+'\t'+str(sum(self.state[3])))
+        print('E\t'+'\t'.join(str(num) for num in self.state[4])+'\t'+str(sum(self.state[4])))
         print()
 
 
     def calculateF1Bonus(self):
         if self.isAllCitiesVisited(): return 100
         return 0
+
+    def calculatePropertiesFromState(self):
+        self.transposed = np.transpose(self.state)
+        self.revenueMatrix = self.calculateRevenueMatrix()
+        self.revenueMatrixTransposed = np.transpose(self.revenueMatrix)
+        self.fbase = self.calculateTotalRevenue()
+        self.f1 = self.calculateF1Bonus()
+        self.f2 = self.calculateF2Bonus()
+        self.f3 = self.calculateF3Bonus()
+        self.f = self.fbase + self.f1 + self.f2 + self.f3
 
     def calculateF2Bonus(self):
         bonuses = []
@@ -54,6 +64,31 @@ class Individual:
         for city in self.transposed:
             if sum(city) == 0: return False 
         return True 
+
+    def mutate(self):
+        temp = np.copy(self.state)
+        city = random.choice(range(5))
+        items = random.sample(range(5), 2)
+
+        # If the element is to be decreased, make sure that it doesn't go below zerp
+        decreaseLimit = self.state[city][items[0]] 
+        # If the element is to be increased, make sure that the other element to be decreased doesn't go below zerp
+        increaseLimit = self.state[city][items[1]]
+        operation = random.choice(['INCREASE', 'DECREASE'])
+        if operation == 'INCREASE':
+            if not increaseLimit == 0:
+                magnitude = random.randint(1, increaseLimit)
+                temp[city][items[0]] += magnitude
+                temp[city][items[1]] -= magnitude
+                self.state = temp
+                self.calculatePropertiesFromState()
+        else:
+            if not decreaseLimit == 0:
+                magnitude = random.randint(1, decreaseLimit)
+                temp[city][items[0]] -= magnitude
+                temp[city][items[1]] += magnitude
+                self.state = temp
+                self.calculatePropertiesFromState()
 
     def printAllProperties(self):
         print('Individual State:')
@@ -85,6 +120,7 @@ def printPopulationState(population):
 def printPopulationFitness(population):
     for index, individual in enumerate(population):
         print(f'Individual {index + 1}:  {individual.f}')
+    print()
     
 def printPopulationProperties(population):
     for individual in population:
@@ -126,7 +162,7 @@ def is150ItemsSold(state):
     return sum(state.flatten()) == 150 
 
 def initializePopulation():
-    states = [generateRandomIndividual() for _ in range(N)]
+    states = [generateRandomIndividual() for _ in range(INITIAL_SIZE)]
     return [Individual(state) for state in states]
 
 def selection(population):
@@ -134,57 +170,73 @@ def selection(population):
     return random.choices(population, fitness, k=N)
 
 def crossover(population):
+    # printPopulationState(population)
     newPopulation = []
     for index in range(N//2):
-        # generate the number of cities to interchange/crossover
+        # generate the number of item types to interchange/crossover
         count = random.randint(1, 4) 
-        print(f'count: {count}')
-        # choose the cities that will be exchanged randomly
-        cities = random.sample(range(5), count)
-        print(f'cities: {cities}')
-        print('before:')
+        # choose the item types that will be exchanged randomly
+        itemTypes = random.sample(range(5), count)
+   
+        temp1 = np.copy(population[index * 2].state)
+        temp2 = np.copy(population[index * 2 + 1].state)
 
-        population[index * 2].printState()
-        population[index * 2 + 1].printState()
+        for type in itemTypes:
+            temp1[type], temp2[type] = temp2[type].copy(), temp1[type].copy()
 
+        newPopulation.append(Individual(temp1))
+        newPopulation.append(Individual(temp2))
+    # print('AFTER')
+    # printPopulationState(newPopulation)
 
-        temp1 = np.copy(population[index * 2].transposed)
-        temp2 = np.copy(population[index * 2 + 1].transposed)
-
-        for city in cities:
-            temp1[city], temp2[city] = temp2[city].copy(), temp1[city].copy()
-
-        newPopulation.append(Individual(np.transpose(temp1)))
-        newPopulation.append(Individual(np.transpose(temp2)))
-        print('after:')
-        newPopulation[index * 2].printState()
-        newPopulation[index * 2 + 1].printState()
-
-        # print(np.transpose(temp1))
-        # print(np.transpose(temp2))
     return newPopulation
 
+def willMutate():
+    return random.choices([True, False], [0.01, 0.98], k=1)
 
-INITIAL_SIZE = 32 # Initial population size
-N = 4 # Initial population size
-LIMIT = 50
-highestFitness = 0
-# bestIndividual = 
+def mutation(population):
+    for individual in population:
+        if willMutate():
+            individual.mutate()
+
+def getBestIndividual(population):
+    ordered = sorted(population, key=lambda individual: individual.f, reverse=True)
+    global bestF, iterationsSinceBest, bestIndividual, iterationCount
+    if ordered[0].f > bestF: 
+        bestF = ordered[0].f
+        bestIndividual = ordered[0]
+        iterationsSinceBest = 0
+    else:
+        iterationsSinceBest += 1
+    iterationCount += 1
+    print(f'Best: {bestF}')
+    print(f'Best of current iteration: {ordered[0].f}')
+    print(f'Iterations: {iterationCount}')
+    print(f'Iterations since best f-score: {iterationsSinceBest}')
+    print()
+
+
+def getNBestIndividual(population):
+    newPopulation = sorted(population, key=lambda individual: individual.f, reverse=True)
+    return newPopulation[:N] 
+         
+INITIAL_SIZE = 128 # Initial population size
+N = 128 # Initial population size
+LIMIT = 250
+bestF = 0
+bestIndividual = None 
 iterationsSinceBest = 0
+iterationCount = 0
 priceMatrix = generatePriceMatrix()
 
+population = getNBestIndividual(initializePopulation())
 
-population = initializePopulation()
-printPopulationState(population)
-printPopulationFitness(population)
-
-selection = selection(population)
-population = crossover(population)
-printPopulationState(population)
-printPopulationFitness(population)
-
-# population = initializePopulation(N)
-# printPopulationState(population)
+while iterationsSinceBest < LIMIT:
+    selected = selection(population)
+    population = crossover(selected)
+    getBestIndividual(population)
+    mutation(population)
+bestIndividual.printProperties()
 
 
 
